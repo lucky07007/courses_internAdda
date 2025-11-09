d38a0f88ff74e89d691d497ab5b35655ef8f2ce1
lucky07007 <lucky7840079250@gmail.com>
2025-11-09 16:28:53 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // The 'content' array tells Tailwind where your HTML/JS files are located.
-  // We specify the 'pages' and 'components' folders.
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  // ... rest of the file
 }
